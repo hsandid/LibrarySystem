2741ffdcf212da6a3e4a5bb54ad9b02fdf53bf2c
hsandid <36267944+hsandid@users.noreply.github.com>
2020-06-20 17:44:23 +0300

    Update lib_secondaryServer.java

--- java/lib_secondaryServer.java
+++ java/lib_secondaryServer.java
@@ -1,12 +1,3 @@
-//Who worked on this part : Ahmad , Farah , Hadi
-//Roles : Ahmad-> Function connecting to the MySQL database and extacting data + database init/values
-//      : Farah -> Socket programming to connect the server/client     
-//      : Hadi -> Utilizing code from the SkillServers file to make the server multithreaded
-
-
-//Used Libraries 
-//Most of them have been imported through examples
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -22,228 +13,208 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 //Primary class Main_Server
-public class Secondary_Server { 
-	
-//final Variables associated to mySQL connections
-static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
-static final String DB_URL = "jdbc:mysql://localhost/libraryalt?useSSL=false";
-static final String USER = "root";
-static final String PASS = "passwordadmin";
+public class Secondary_Server {
+
+    //final Variables associated to mySQL connections
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/libraryalt?useSSL=false";
+    static final String USER = "root";
+    static final String PASS = "passwordadmin";
 
 
- //Creating a serverSocket for the connection
-private static ServerSocket welcomeSocket;	
+    //Creating a serverSocket for the connection
+    private static ServerSocket welcomeSocket;
 
 
-//Init the mySQL driver
-public Secondary_Server() throws ClassNotFoundException
-{
-	Class.forName("com.mysql.jdbc.Driver");
-}
-
-//function acceptConnections does most of the work associated with multithreading / socket prog
-public void acceptConnections() {
-
-	try {
-		  
-		  welcomeSocket = new ServerSocket(3680); 
-		  
-		     }
-		     catch (IOException e) {
-		       System.err.println("ServerSocket instantiation failure");
-		       e.printStackTrace();
-		       System.exit(0);
-		     }
-    
-    
-    while(true) { 
-  	  
-  	  try {
-  		  Socket newConnection = welcomeSocket.accept(); 
-  		  ServerThread st = new ServerThread(newConnection);
-  		  new Thread(st).start();
-  	  }
-  	  catch (IOException ioe) {
-  	        System.err.println("server accept failed");
-  	      }
-}
-}
-
-
-
-  public static void main(String argv[]) throws Exception 
-    { 
-	  
-	  Secondary_Server server = null;
-	   try {
-	       //Instantiate an object of this class. This will load the JDBC database driver
-	      server = new Secondary_Server();
-	   }
-	    catch (ClassNotFoundException e) {
-	      System.out.println("unable to load JDBC driver");
-	      e.printStackTrace();
-	      System.exit(1);
-	    }
-	   
-	   server.acceptConnections();
+    //Init the mySQL driver
+    public Secondary_Server() throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
     }
-  
-  // Socket programming & MYSQL datafetch
-	  
-  class ServerThread implements Runnable {
 
-	  private Socket connectionSocket;
-	  private ArrayList<String> results = new ArrayList<String>() ;
-	    
+    //function acceptConnections does most of the work associated with multithreading / socket prog
+    public void acceptConnections() {
 
-	    public ServerThread(Socket socket) {
-	    //Inside the constructor: store the passed object in the data member  
-	      this.connectionSocket = socket;
-	    }
+        try {
 
-	 //This is where you place the code you want to run in a thread
-	 //Every instance of a ServerThread will handle one client (TCP connection)
-	    public void run() {
-	    	
-	    	 String clientSentence; 
-	         String mySentence; 
-	         
-	         Connection conn = null;
-	   	 		Statement stmt = null;
-	             
-	              
-	              //Initializing input / output streams
+            welcomeSocket = new ServerSocket(3680);
 
-	   	ObjectOutputStream outToClient = null;
-				try {
-					outToClient = new ObjectOutputStream(connectionSocket.getOutputStream());
-				} catch (IOException e2) {
-					// TODO Auto-generated catch block
-					e2.printStackTrace();
-				} 
+        } catch (IOException e) {
+            System.err.println("ServerSocket instantiation failure");
+            e.printStackTrace();
+            System.exit(0);
+        }
 
-        
-	             ObjectInputStream inFromClient = null;
-				try {
-					inFromClient = new ObjectInputStream(connectionSocket.getInputStream());
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} 
-        
-          //Read the sentence sent by the user
-	            String[] userQuery = new String[3];
-	            String outString = null;
-	            try {
-					userQuery = (String[]) inFromClient.readObject();
-				} catch (ClassNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}             
-	            
-	            if (userQuery[0].startsWith("searchBook"))
-	            {
-	            	results.clear();
-	            	 // Initializing the connection the database and fetching the requested data 
-	            	try {
-						conn = DriverManager.getConnection(DB_URL,USER,PASS);
-						  stmt = conn.createStatement();
-			       	      String sql="";
-			       	      System.out.println(sql);
-			       	      sql="SELECT title FROM books";
-			       	      
-			       	      ResultSet rs = stmt.executeQuery(sql);
-			       	      
-			       	      
-			       	      System.out.println(userQuery[1]);
-			       	      
-			       	      
-			       	      
-			       	      while (rs.next())
-			       	      {
-			       	    	  String tempTitle = rs.getString("title");
-			       	    	if(tempTitle.toLowerCase().contains(userQuery[1].toLowerCase()))
-			       	    	{
-			       	    		results.add(tempTitle+" (FROM SECONDARY CATALOG)");
-			       	    	}
-			       	      }
-			       
-			       	    	  
-			       	      
-			       	 
-			       	      
-			       	      rs.close();
-			       	      stmt.close();
-			       	      conn.close();
-						
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-		       	    
-	            	 try {
-	 					outToClient.writeObject(results);
-	 				} catch (IOException e) {
-	 					// TODO Auto-generated catch block
-	 					e.printStackTrace();
-	 				 
 
-	            }
-	            	    
-  }
-	            
-	            if (userQuery[0].startsWith("getBooks"))
-	            {
-	            	
-	            	 // Initializing the connection the database and fetching the requested data 
-	            	try {
-						conn = DriverManager.getConnection(DB_URL,USER,PASS);
-						  stmt = conn.createStatement();
-			       	      String sql="";
-			       	      System.out.println(sql);
-			       	      sql="SELECT title,date_of_pub FROM books WHERE genre='"+userQuery[1]+"'";
-			       	      
-			       	      ResultSet rs = stmt.executeQuery(sql);
-			       	      results.clear();
-			       	      
-			       	   while(rs.next()){
-			       	          
-			       	    	  String temp;
-			       	    	
-			       	          //Concatinating the data into a single string
-			       	          String title = rs.getString("title");
-			       	          int date = rs.getInt("date_of_pub");
-			       	          temp=(title+" ; "+date+" (FROM SECONDARY CATALOG)");
-		                  //storing the data into the array of string myList
-			       	          results.add(temp);
-			       	          
-			       	          
-			       	      
-			       		}
-			       	      
-			       	      rs.close();
-			       	      stmt.close();
-			       	      conn.close();
-						
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-		       	    
-	            	 try {
-	 					outToClient.writeObject(results);
-	 				} catch (IOException e) {
-	 					// TODO Auto-generated catch block
-	 					e.printStackTrace();
-	 				} 
+        while (true) {
 
-	            }
-  }
+            try {
+                Socket newConnection = welcomeSocket.accept();
+                ServerThread st = new ServerThread(newConnection);
+                new Thread(st).start();
+            } catch (IOException ioe) {
+                System.err.println("server accept failed");
+            }
+        }
+    }
+
+
+
+    public static void main(String argv[]) throws Exception {
+
+        Secondary_Server server = null;
+        try {
+            //Instantiate an object of this class. This will load the JDBC database driver
+            server = new Secondary_Server();
+        } catch (ClassNotFoundException e) {
+            System.out.println("unable to load JDBC driver");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        server.acceptConnections();
+    }
+
+    // Socket programming & MYSQL datafetch
+
+    class ServerThread implements Runnable {
+
+        private Socket connectionSocket;
+        private ArrayList < String > results = new ArrayList < String > ();
+
+
+        public ServerThread(Socket socket) {
+            //Inside the constructor: store the passed object in the data member  
+            this.connectionSocket = socket;
+        }
+
+        //This is where you place the code you want to run in a thread
+        //Every instance of a ServerThread will handle one client (TCP connection)
+        public void run() {
+
+            String clientSentence;
+            String mySentence;
+
+            Connection conn = null;
+            Statement stmt = null;
+
+
+            //Initializing input / output streams
+
+            ObjectOutputStream outToClient = null;
+            try {
+                outToClient = new ObjectOutputStream(connectionSocket.getOutputStream());
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+
+
+            ObjectInputStream inFromClient = null;
+            try {
+                inFromClient = new ObjectInputStream(connectionSocket.getInputStream());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+            //Read the sentence sent by the user
+            String[] userQuery = new String[3];
+            String outString = null;
+            try {
+                userQuery = (String[]) inFromClient.readObject();
+            } catch (ClassNotFoundException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+            if (userQuery[0].startsWith("searchBook")) {
+                results.clear();
+                // Initializing the connection the database and fetching the requested data 
+                try {
+                    conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                    stmt = conn.createStatement();
+                    String sql = "";
+                    System.out.println(sql);
+                    sql = "SELECT title FROM books";
+
+                    ResultSet rs = stmt.executeQuery(sql);
+
+
+                    System.out.println(userQuery[1]);
+
+
+
+                    while (rs.next()) {
+                        String tempTitle = rs.getString("title");
+                        if (tempTitle.toLowerCase().contains(userQuery[1].toLowerCase())) {
+                            results.add(tempTitle + " (FROM SECONDARY CATALOG)");
+                        }
+                    }
+
+
+
+
+
+                    rs.close();
+                    stmt.close();
+                    conn.close();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    outToClient.writeObject(results);
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+
+                }
+
+            }
+
+            if (userQuery[0].startsWith("getBooks")) {
+
+                // Initializing the connection the database and fetching the requested data 
+                try {
+                    conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                    stmt = conn.createStatement();
+                    String sql = "";
+                    System.out.println(sql);
+                    sql = "SELECT title,date_of_pub FROM books WHERE genre='" + userQuery[1] + "'";
+
+                    ResultSet rs = stmt.executeQuery(sql);
+                    results.clear();
+
+                    while (rs.next()) {
+
+                        String temp;
+
+                        //Concatinating the data into a single string
+                        String title = rs.getString("title");
+                        int date = rs.getInt("date_of_pub");
+                        temp = (title + " ; " + date + " (FROM SECONDARY CATALOG)");
+                        //storing the data into the array of string myList
+                        results.add(temp);
+
+
+
+                    }
+
+                    rs.close();
+                    stmt.close();
+                    conn.close();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    outToClient.writeObject(results);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
 }
-}
-
-
-  
