641728db5a61d3c713698151f2dfbf897e5b5ea6
Hadi Sandid <36267944+hsandid@users.noreply.github.com>
2020-08-16 02:28:26 +0300

    Update primaryServer.java

--- java/primaryServer.java
+++ java/primaryServer.java
@@ -12,8 +12,7 @@ import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-//Primary class Main_Server
-public class Main_Server {
+public class primaryServer {
 
     //final Variables associated to mySQL connections
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
